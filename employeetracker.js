8b1eb580ff5f6b87d67225988e670c0543e39a53
Alexandra Grassl <eai.alexandra@gmail.com>
2021-07-12 14:26:40 -0500

    add dept, view dept, and add role added

--- employeetracker.js
+++ employeetracker.js
@@ -30,7 +30,7 @@ const employeeOptions = [
     "exit"
 ];
 
-const runSearch = () => {
+const optionPrompt = () => {
   inquirer
     .prompt({
       name: 'action',
@@ -91,118 +91,34 @@ const addDepartment = () => {
       message: 'What department would you like to add ?',
     })
     .then((answer) => {
-      const query = 'SELECT id, name FROM top5000 WHERE ?';
-      connection.query(query, { artist: answer.artist }, (err, res) => {
-        res.forEach(({ position, song, year }) => {
-          console.log(
-            `Position: ${position} || Song: ${song} || Year: ${year}`
-          );
-        });
-        runSearch();
+      const query = 'INSERT INTO department (name) Values ( ? )';
+      connection.query(query, { department: answer.department }, (err, res) => {
+        viewDepartments();
       });
     });
 };
 
-const multiSearch = () => {
+const viewDepartments = () => {
   const query =
-    'SELECT artist FROM top5000 GROUP BY artist HAVING count(*) > 1';
+    'SELECT * FROM department';
   connection.query(query, (err, res) => {
-    res.forEach(({ artist }) => console.log(artist));
-    runSearch();
+    res.forEach(({ department }) => console.log(department));
+    optionPrompt();
   });
 };
 
-const rangeSearch = () => {
-  inquirer
-    .prompt([
-      {
-        name: 'start',
+const addRole = () => {
+    inquirer
+      .prompt({
+        name: 'role',
         type: 'input',
-        message: 'Enter starting position: ',
-        validate(value) {
-          if (isNaN(value) === false) {
-            return true;
-          }
-          return false;
-        },
-      },
-      {
-        name: 'end',
-        type: 'input',
-        message: 'Enter ending position: ',
-        validate(value) {
-          if (isNaN(value) === false) {
-            return true;
-          }
-          return false;
-        },
-      },
-    ])
-    .then((answer) => {
-      const query =
-        'SELECT position,song,artist,year FROM top5000 WHERE position BETWEEN ? AND ?';
-      connection.query(query, [answer.start, answer.end], (err, res) => {
-        res.forEach(({ position, song, artist, year }) => {
-          console.log(
-            `Position: ${position} || Song: ${song} || Artist: ${artist} || Year: ${year}`
-          );
+        message: 'What role would you like to add ?',
+      })
+      .then((answer) => {
+        const query = 'INSERT INTO role (name) Values ( ? )';
+        connection.query(query, { role: answer.role }, (err, res) => {
+          viewRoles();
         });
-        runSearch();
       });
-    });
-};
+  };
 
-const songSearch = () => {
-  inquirer
-    .prompt({
-      name: 'song',
-      type: 'input',
-      message: 'What song would you like to look for?',
-    })
-    .then((answer) => {
-      console.log(answer.song);
-      connection.query(
-        'SELECT * FROM top5000 WHERE ?',
-        { song: answer.song },
-        (err, res) => {
-          if (res[0]) {
-            console.log(
-              `Position: ${res[0].position} || Song: ${res[0].song} || Artist: ${res[0].artist} || Year: ${res[0].year}`
-            );
-          } else {
-            console.error(`No results for ${answer.song}`);
-          }
-          runSearch();
-        }
-      );
-    });
-};
-
-const songAndAlbumSearch = () => {
-  inquirer
-    .prompt({
-      name: 'artist',
-      type: 'input',
-      message: 'What artist would you like to search for?',
-    })
-    .then((answer) => {
-      let query =
-        'SELECT top_albums.year, top_albums.album, top_albums.position, top5000.song, top5000.artist ';
-      query +=
-        'FROM top_albums INNER JOIN top5000 ON (top_albums.artist = top5000.artist AND top_albums.year ';
-      query +=
-        '= top5000.year) WHERE (top_albums.artist = ? AND top5000.artist = ?) ORDER BY top_albums.year, top_albums.position';
-
-      connection.query(query, [answer.artist, answer.artist], (err, res) => {
-        console.log(`${res.length} matches found!`);
-        res.forEach(({ year, position, artist, song, album }, i) => {
-          const num = i + 1;
-          console.log(
-            `${num} Year: ${year} Position: ${position} || Artist: ${artist} || Song: ${song} || Album: ${album}`
-          );
-        });
-
-        runSearch();
-      });
-    });
-};
